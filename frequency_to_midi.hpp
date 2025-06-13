b5c5bc396b9633885eaa244968dd4446ee8eb8dc
yenaii <andrejsuslov1441@gmail.com>
2025-06-13 23:40:30 +0300

    freq to midi 1

---
+++ frequency_to_midi.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+#include <utility>
+
+const int min_midi = 40;
+const int max_midi = 86;
+
+struct Triple {
+    int num;
+    int midi;
+    double duration;
+};
+
+int frequency_to_midi(double freq);
+double duration_sum(const std::vector<Triple>& midi_duration);
+int max_duration(const std::vector<Triple>& midi_duration);
+std::vector<std::pair<int, int>> get_midi_beats(std::vector<std::pair<double, double>> frequency_duration, int bpm);
